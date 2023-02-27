24db96e0a7636948b0624125c298e09327acb486
saransh-khobragade <saransh98@gmail.com>
2023-02-28 00:42:58 +0530

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,6 @@ import React,{useEffect} from 'react';
 import axios from "axios";
 
 export default function Home() {
-
-  const getCallURL = process.env.getCallURL
-  
-  useEffect(()=>{
-    axios.get(getCallURL).then((response) => {
-      console.log(response.data);
-    });
-  })
   return (
     <div>
       Hello React App
